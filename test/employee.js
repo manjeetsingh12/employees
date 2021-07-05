706510be9aa1bdb553dcf8cf872d9d28e39fd838
manjeet <manjeet.singh@sparxitsolutions.com>
2021-07-05 13:26:05 +0530

    changes

--- test/employee.js
+++ test/employee.js
@@ -1,12 +1,10 @@
 //Require the dev-dependencies
 let chai = require("chai");
-let chaiHttp = require("chai-http");
 let server = require("../server");
-var request = require("request");
+const request = require("request");
 const should = chai.should();
 const expect = chai.expect;
 const assert = chai.assert;
-chai.use(chaiHttp);
 let { result } = require("../helpers/result");
 
 /*
@@ -18,8 +16,6 @@ describe("/GET getEmployeesDepartments", () => {
     request(
       "http://localhost:3015/getEmployeesDepartments",
       (error, response, body) => {
-        console.log(response);
-
         expect(response.statusCode).to.equal(200);
         expect(body).should.be.a("object");
         assert.equal(response.body, JSON.stringify(result));
